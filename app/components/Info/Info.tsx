c36d20d3cddc27f4f50e7df2297c0917911d29ca
NicolasNewman <nanewman01@gmail.com>
2019-09-21 19:17:46 -0400

    started implementing form

--- app/components/Info/Info.tsx
+++ app/components/Info/Info.tsx
@@ -1,10 +1,60 @@
 import * as React from 'react';
 import { Component } from 'react';
+import { Form, Input, Button, Icon, Row } from 'antd';
+import { FormComponentProps } from 'antd/lib/form/Form';
 
-interface IProps {}
+interface IProps extends FormComponentProps {}
 
-export default class Info extends Component<IProps> {
+class Info extends Component<IProps> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
     render() {
-        return <h1>Hello</h1>;
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div className="form">
+                <Form layout={'inline'} onSubmit={this.handleSubmit}>
+                    <Form.Item label="API Key">
+                        {getFieldDecorator('key', {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: 'Please input an API key'
+                                }
+                            ]
+                        })(
+                            <Input
+                                prefix={
+                                    <Icon
+                                        type="user"
+                                        style={{ color: 'rgba(0,0,0,0.25)' }}
+                                    />
+                                }
+                                placeholder="API Key"
+                            />
+                        )}
+                    </Form.Item>
+                    <Row>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Row>
+                </Form>
+            </div>
+        );
     }
 }
+
+export default Form.create<IProps>({})(Info);
